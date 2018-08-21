7e0ddb7673de0e20c695f2499742fc34720045ad
唐益萍 <tangyiping@memect.co>
2018-08-21 13:46:56 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    add = ()=>{
+        console.log(111)
+    }
+
+
+    render() {
+        const a = 0;
+
+        return (
+            <div classname="App">
+                111
+            </div>
+        );
+    }
+}
+
+export default App;
